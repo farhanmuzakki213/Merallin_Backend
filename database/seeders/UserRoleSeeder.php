224bd5b33c008ad3347a3dc7222e1170fa30c1a6
farhanmuzakki213 <farhanmuzakki1p@gmail.com>
2025-08-04 11:15:34 +0700

    first commit

---
+++ database/seeders/UserRoleSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create roles if they don't exist
+        $role_karyawan = Role::firstOrCreate(['name' => 'karyawan']);
+        $role_driver = Role::firstOrCreate(['name' => 'driver']);
+
+        // Create Karyawan User
+        $karyawan = User::factory()->create([
+            'name' => 'Karyawan User',
+            'email' => 'karyawan@example.com',
+            'password' => Hash::make('password')
+        ]);
+        $karyawan->assignRole($role_karyawan);
+
+        // Create Driver User
+        $driver = User::factory()->create([
+            'name' => 'Driver User',
+            'email' => 'driver@example.com',
+            'password' => Hash::make('password')
+        ]);
+        $driver->assignRole($role_driver);
+    }
+}
