e1bf30e1310f4efb7c48bc361008e6f5f67b8b84
farhanmuzakki213 <farhanmuzakki1p@gmail.com>
2025-08-06 16:37:55 +0700

    fix:model4

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,7 +52,7 @@ class User extends Authenticatable
         ];
     }
 
-    public function attendance()
+    public function attendances()
     {
         return $this->hasMany(Attendance::class);
     }
